5530d733ef7d82afd5cd8507a91aea9f6c0b58aa
Joaquin <aguirrejoaquin17@gmail.com>
2023-09-21 19:21:01 -0300

    Java

---
+++ JAVA/logica.js
@@ -0,0 +1,65 @@
+console.table(productos);
+let carrito = [];
+
+
+
+const contenedorProds = document.getElementById('misprods');
+const tablaBody = document.getElementById('tablabody');
+
+//DOM
+function renderizarProds(listaProds) {
+    for (const prod of listaProds) {
+        contenedorProds.innerHTML += `
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src=${prod.foto} alt=${prod.nombre}/>
+                    <div class="card-body">
+                        <h5 class="card-title">${prod.nombre}</h5>
+                        <p class="card-text">$ ${prod.precio}</p>
+                        <button id=${prod.id} class="btn btn-primary compra">Comprar</button>
+                    </div>
+            </div>
+        `;
+    }
+
+    //eventos
+    let botones = document.getElementsByClassName('compra');//coleccion de nodos
+    for (const boton of botones) {
+        //opcion 1 - addEventListener
+        boton.addEventListener('click', () => {
+            console.log('Hiciste click en el boton cuyo id es ' + boton.id);
+            const prodACarro = listaProds.find((producto) => producto.id == boton.id);
+            console.log(prodACarro);
+            //cargar producto encontrado al carro
+            agregarAlCarrito(prodACarro);
+        });
+
+        
+    }
+}
+
+renderizarProds(productos);
+
+
+function agregarAlCarrito(producto) {
+    carrito.push(producto);
+    console.table(carrito);
+    alert(`Agregaste ${producto.nombre} al carro 🛒`);
+    //agregar el producto a la tabla
+    tablaBody.innerHTML += `
+        <tr>
+            <td>${producto.id}</td>
+            <td>${producto.nombre}</td>
+            <td>${producto.precio}</td>
+        </tr>
+    `;
+    //calcular el total gastado hasta el momento
+    
+    total.innerHTML += `
+        <tr>
+            <td>${producto.precio }</td>
+        </tr>
+    `;
+
+}
+
+
---
+++ JAVA/productos.js
@@ -0,0 +1,43 @@
+const productos = [{
+        id: 1,
+        foto: "https://www.fichasmig.com.ar/wp-content/uploads/2020/11/fichas-mig-min.png",
+        nombre: "Ficha macho + hembra ",
+        precio: 2000
+    },
+    {
+        id: 2,
+        foto: "https://img.directindustry.es/images_di/photo-g/5067-11648077.jpg",
+        nombre: "Caja termica ",
+        precio: 10000
+    },
+    {
+        id: 3,
+        foto: "https://tuboplastperu.com/wp-content/uploads/2020/08/Caja-rectangular.jpg",
+        nombre: " Caja de pared",
+        precio: 1600
+    },
+    {
+        id: 4,
+        foto: "https://http2.mlstatic.com/D_NQ_NP_613096-MLA49152041284_022022-O.webp",
+        nombre: "Toma corriente ",
+        precio: 1200
+    },
+    {
+        id: 5,
+        foto: "https://img.interempresas.net/FotosArtProductos/P168757.jpg",
+        nombre: "Caja electrica",
+        precio: 2400
+    },
+    {
+        id: 6,
+        foto: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/660/566/products/electro15negro1-f751d11391e009463416662696795763-480-0.jpg",
+        nombre: "Cable 20mm",
+        precio: 3400
+    },
+    {
+        id: 7,
+        foto: "https://www.roker.com.ar/wp-content/uploads/2021/01/CABLECANAL-100x50-1.png",
+        nombre: "Cable canal ",
+        precio: 200
+    }
+];
